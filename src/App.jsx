62118ca8196f26987aadaee2fda86136f232f19d
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2024-12-03 09:09:10 +0500

    tugatdim

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// layouts
+import MainLayouts from "./layouts/MainLayouts";
+
+// pages
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import ReadTodo from "./pages/ReadTodo";
+
+const getTodosLocalStorage = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+function App() {
+  const [todos, setTodos] = useState(getTodosLocalStorage());
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  const editTodoChange = (to) => {
+    setTodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id == to.id) {
+          return { ...to };
+        } else {
+          return todo;
+        }
+      });
+    });
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayouts />,
+      children: [
+        {
+          index: true,
+          element: (
+            <Home
+              editTodoChange={editTodoChange}
+              todos={todos}
+              setTodos={setTodos}
+            />
+          ),
+        },
+        {
+          path: "/create",
+          element: <Create setTodos={setTodos} />,
+        },
+        {
+          path: "/read/:id",
+          element: <ReadTodo todos={todos} />,
+        },
+      ],
+    },
+  ]);
+  return (
+    <>
+      <div className="flex-col-reverse">
+        <RouterProvider router={router} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoTrashBin } from "react-icons/io5";
+import { MdModeEditOutline } from "react-icons/md";
+import Madal from "../components/Madal";
+import { useState } from "react";
+
+function Home({ todos, setTodos, editTodoChange }) {
+  const [todoId, setTodoId] = useState(null);
+  const [todo, setTodo] = useState(null);
+  const todosDelete = (id) => {
+    setTodos((prev) => {
+      return prev.filter((us) => {
+        return us.id !== id;
+      });
+    });
+  };
+  return (
+    <>
+      <div className="mx-auto my-10 grid max-w-5xl">
+        <ul className="grid grid-cols-3 gap-8">
+          <Madal t={todo} editTodoChange={editTodoChange} />
+          {todos.map((todo) => {
+            return (
+              <li
+                key={todo.id}
+                className="card w-full bg-base-100 shadow duration-500"
+              >
+                <div className="card-body">
+                  <h2 className="card-title">{todo.title}</h2>
+                  <p>{todo.description.slice(0, 60)}...</p>
+                  <div className="card-actions items-center justify-end">
+                    <button
+                      onClick={() => {
+                        setTodoId(todo.id);
+                        setTodo(todo);
+                        document.getElementById("my_modal_5").showModal();
+                      }}
+                      className="btn btn-primary btn-sm duration-500"
+                    >
+                      <MdModeEditOutline />
+                    </button>
+                    <Link
+                      to={`/read/${todo.id}`}
+                      className="btn btn-primary btn-sm duration-500"
+                    >
+                      Read
+                    </Link>
+                    <IoTrashBin
+                      onClick={() => todosDelete(todo.id)}
+                      className="icon h-5 w-5 duration-500"
+                    />
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Home;
